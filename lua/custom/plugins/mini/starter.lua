4044be54b8aca33171e55cbd236c56019b57d4c7
François Vantomme <francois.vantomme@enercoop.org>
2024-12-14 01:24:40 +0100

    feat(starter): add plugin actions & more tweaks

--- lua/custom/plugins/mini/starter.lua
+++ lua/custom/plugins/mini/starter.lua
@@ -3,11 +3,20 @@ local top3 = function()
 
   local command = 'git shortlog -ns --since "1 year" | head -n 3'
   local handle = io.popen(command)
-  local shortlog = handle:read '*a' or 'not a git directory'
+
+  ---@cast handle -nil
+  local shortlog = handle:read '*a'
   handle:close()
 
   return table.concat({ label, shortlog }, '\n')
 end
+
+local plugin_actions = {
+  { name = 'Kickstart', action = 'e ~/.config/nvim/init.lua', section = 'Plugin actions' },
+  { name = 'Lazy', action = 'Lazy', section = 'Plugin actions' },
+  { name = 'Mason', action = 'Mason', section = 'Plugin actions' },
+  { name = 'Health', action = 'checkhealth', section = 'Plugin actions' },
+}
 
 local starter = require 'mini.starter'
 
@@ -18,14 +27,15 @@ return {
     evaluate_single = true,
     header = top3,
     items = {
-      starter.sections.recent_files(8, true),
-      starter.sections.recent_files(8, false),
+      starter.sections.recent_files(5, true),
+      starter.sections.recent_files(5, false),
       starter.sections.telescope(),
+      plugin_actions,
       starter.sections.builtin_actions(),
     },
     content_hooks = {
       starter.gen_hook.adding_bullet(),
-      starter.gen_hook.indexing('all', { 'Builtin actions', 'Telescope' }),
+      starter.gen_hook.indexing('all', { 'Builtin actions', 'Plugin actions', 'Telescope' }),
       starter.gen_hook.aligning('center', 'center'),
     },
     query_updaters = 'abcdefghijklmnopqrstuvwxyz0123456789',
